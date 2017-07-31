ba179af4788ff37101727bb702467261a60cbec3
Cyrill <lichunyang@outlook.com>
2017-07-31 18:09:20 +0800

    cc

--- djangoLearn/mysite/mysite/urls.py
+++ djangoLearn/mysite/mysite/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from learn import views as learn_views
 
 urlpatterns = [
-    url(r'^$', learn_views.index),
     url(r'^admin/', admin.site.urls),
 ]
